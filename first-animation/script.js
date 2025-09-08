1f96c99cbcb24d881e01752474a1a932a43a8223
Kamil <ahmaadm84112@gmail.com>
2025-09-08 19:39:02 +0700

    Fix: Update card rotation to 120 degrees and add box shadow effect

--- first-animation/script.js
+++ first-animation/script.js
@@ -3,9 +3,10 @@ gsap.to(".card", {
     x: -100,
     opacity: 1,
     duration: 2,
-    rotation: 360,
     borderRadius: "50%",
     ease: 'power1.inOut',
     repeat: -1,
+    rotation: 120,
+    boxShadow: "0 0 20px rgba(0, 255, 255, 0.8)",
     yoyo: true,
 })
